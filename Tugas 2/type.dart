6def0f21c6753254ccb0de37a42c17fae28c27b5
Erdeatha R. Putra <rputraramdani2010@gmail.com>
2025-07-04 15:12:54 +0700

    Upload Tugas 2 Type Data

---
+++ Tugas 2/type.dart
@@ -0,0 +1,10 @@
+void main() {
+  String variable1 = "MAQDIS";
+  String variable2 = 'Academy';
+  String variable3 = 'is';
+  String variable4 = 'awesome';
+
+  String output = '$variable1 $variable2 $variable3 $variable4';
+
+  print(output);
+}
